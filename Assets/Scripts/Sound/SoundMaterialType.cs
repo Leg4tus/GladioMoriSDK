d4aaeb44f54a106ac87ed01e1573eec61a41a457
Leg4tus <joonas.pirttikangas@gmail.com>
2026-09-13 11:52:41 +0300

    MOD: New SoundMaterialTypes for 1.0 version of the game.
    - Ground sound materials allow for separate footstep sounds

--- Assets/Scripts/Sound/SoundMaterialType.cs
+++ Assets/Scripts/Sound/SoundMaterialType.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public enum SoundMaterialType
 {
-    Metal,
-    Wood,
-    Player
+    Metal = 0,
+    Wood = 1,
+    Player = 2,
+    Foot = 3,
+    Hand = 4,
+
+
+    Ground = 900,
+    GroundWood = 901,
+    GroundStone = 902,
 }
